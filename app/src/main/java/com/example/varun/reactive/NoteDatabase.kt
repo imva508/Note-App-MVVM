d05bb579531bc365908640408d166f636abc973b
varun <imva508@gmail.com>
2018-12-14 22:47:23 +0530

    created database

--- app/src/main/java/com/example/varun/reactive/NoteDatabase.kt
+++ app/src/main/java/com/example/varun/reactive/NoteDatabase.kt
@@ -1,11 +1,30 @@
 package com.example.varun.reactive
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import kotlin.reflect.KClass
 
 
-@Database(entities = [Note::class],version = 1)
- abstract class NoteDatabase :RoomDatabase(){
+@Database(entities = [Note::class], version = 1)
+abstract class NoteDatabase : RoomDatabase() {
+
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+
+        private lateinit var instance: NoteDatabase
+
+        private fun getInstance(context: Context): NoteDatabase {
+            if (instance == null)
+                instance = Room.databaseBuilder(context.applicationContext, NoteDatabase::class.java, "note_database")
+                    .fallbackToDestructiveMigration().build()
+            return instance
+        }
+
+
+
+    }
 
 }
